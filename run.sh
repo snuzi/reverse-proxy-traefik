a92ffb8a688d4d74a0ae7b4540d4abf386b3ba71
snuzi <sabrinuzi@gmail.com>
2019-10-09 15:39:25 +0200

    Initial sample

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+IFS=$'\n\t'
+
+docker-compose up -d
+
+echo "Will now ask for root access to add app1.test, app2.test and app3.test to /etc/hosts\n"
+
+sudo bash -c 'echo "127.0.0.1 app1.test" >> /etc/hosts'
+sudo bash -c 'echo "127.0.0.1 app2.test" >> /etc/hosts'
+sudo bash -c 'echo "127.0.0.1 app3.test" >> /etc/hosts'
+
+curl http://app1.test/
+curl http://app2.test/
+curl http://app3.test/
